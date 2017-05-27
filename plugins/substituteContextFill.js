1593ae663e6bc690c7edc193c2e18eaff3d8273d
Sam Foster <sfoster@mozilla.com>
2017-05-26 17:33:00 -0700

    First pass at a plugin to match and replace fill attributes with context-fill

---
+++ plugins/substituteContextFill.js
@@ -0,0 +1,24 @@
+'use strict';
+/*eslint strict:0*/
+
+exports.type = 'perItem';
+
+exports.active = true;
+
+exports.description = 'removes moz-user-select style property';
+
+var re = /-moz-user-select:\s*none;?/;
+exports.fn = function(item, params) {
+  if (params && params.fill) {
+    let fillColor = params.fill;
+    console.log('got fill param: ', fillColor);
+    if (item.elem && item.hasAttr('fill')) {
+      let attr = item.attr('fill');
+      // TODO: could normalize to hex triplet
+      if (attr.value === fillColor) {
+        attr.value = 'context-fill';
+      }
+    }
+  }
+  return true;
+}
