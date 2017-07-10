029cbaba762d4e903da4f5d112510569b4d1042c
Sam Foster <sfoster@mozilla.com>
2017-07-10 15:19:08 -0700

    Support a list of color values in the substituteContextFill plugin. Use better defaults for convertPathData

--- plugins/substituteContextFill.js
+++ plugins/substituteContextFill.js
@@ -5,18 +5,20 @@ exports.type = 'perItem';
 
 exports.active = true;
 
-exports.description = 'removes moz-user-select style property';
+exports.description = 'Replace matching fill attribute values with "context-fill"';
 
 var re = /-moz-user-select:\s*none;?/;
 exports.fn = function(item, params) {
   if (params && params.fill) {
-    let fillColor = params.fill;
-    // console.log('got fill param: ', fillColor);
+    let fillColors = typeof params.fill === 'string' ? [params.fill] : params.fill;
+    // console.log('got fill param: ', fillColors);
     if (item.elem && item.hasAttr('fill')) {
       let attr = item.attr('fill');
       // TODO: could normalize to hex triplet
-      if (attr.value === fillColor) {
+      if (fillColors.includes(attr.value)) {
         attr.value = 'context-fill';
+      } else {
+        console.warn('no substitute for :', attr.value);
       }
     }
   }
